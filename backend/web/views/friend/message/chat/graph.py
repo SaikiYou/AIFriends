f828c369569a1eca551347cc9f0cdfeb62954934
河中第一深情 <2746541792@qq.com>
2026-06-14 11:39:30 +0800

    实现function call

--- backend/web/views/friend/message/chat/graph.py
+++ backend/web/views/friend/message/chat/graph.py
@@ -1,15 +1,26 @@
 import os
 from typing import TypedDict, Annotated, Sequence
 
+from django.utils.timezone import now, localtime
 from langchain_core.messages import BaseMessage
+from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langgraph.constants import START, END
 from langgraph.graph import add_messages, StateGraph
+from langgraph.prebuilt import ToolNode
+
 
 # 定义大模型
 class ChatGraph:
     @staticmethod
     def create_app():
+        @tool
+        def get_time() -> str:
+            '''当需要获取当前时间时候调用此函数返回格式[YYYY-MM-DD HH:MM:SS]'''
+            return localtime(now()).strftime('%Y-%m-%d %H:%M:%S')
+
+        tools = [get_time]
+
         llm = ChatOpenAI(
             model = 'qwen3.6-plus',
             openai_api_key = os.getenv('API_KEY'),
@@ -21,7 +32,7 @@ class ChatGraph:
                 "include_usage": True,  # 输出token消耗数量
             }
         }
-        )
+        ).bind_tools(tools)
         # 定义状态维护大模型
         class AgentState(TypedDict):
             messages: Annotated[Sequence[BaseMessage], add_messages]
@@ -29,11 +40,29 @@ class ChatGraph:
         def model_call(state:AgentState) -> AgentState:
             res = llm.invoke(state['messages'])
             return {'messages': [res]}
+
+        def should_continue(state:AgentState) -> str:
+            last_message = state['messages'][-1]
+            if last_message.tool_calls:
+                return 'tools'
+            return 'end'
+
+        tool_node = ToolNode(tools)
+
         # 计算流程图
         graph = StateGraph(AgentState)
         graph.add_node('agent', model_call)
+        graph.add_node('tools', tool_node)
 
         graph.add_edge(START, 'agent')
-        graph.add_edge('agent', END)
+        graph.add_conditional_edges(
+            'agent',
+            should_continue,
+            {
+                'tools': 'tools',
+                'end': END
+            }
+        )
+        graph.add_edge('tools', 'agent')
 
         return  graph.compile()
